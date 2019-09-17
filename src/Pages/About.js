dc08d771c262cf6da46f3012244f18a13e9fcf0c
mosvisio <lucas.schutz0954@gmail.com>
2019-09-17 13:51:35 +0200

    link in about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -12,7 +12,7 @@ const About = () => {
     return (
         <animated.div style={props} className="shadow about-content">
             <h1>About me</h1>
-            <i class="fab fa-linkedin"></i> <i class="fab fa-github"></i>
+            <p className="icon"><a href="https://www.linkedin.com/in/lucas-schutz-208883143/" rel="noopener noreferrer" target="_blank"><i class="fab fa-linkedin"></i></a> <a href="https://github.com/MOSvisio" rel="noopener noreferrer" target="_blank"><i class="fab fa-github"></i></a></p>
             <p>
             Praesent interdum ullamcorper mauris in hendrerit. Mauris dapibus nisi et augue luctus, at convallis arcu facilisis. Donec sem tellus, tincidunt id ipsum sit amet, consequat semper risus. Donec eu tincidunt eros. Nunc sollicitudin augue sit amet mi ultricies mollis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer dui arcu, finibus vel lacus ut, tristique viverra nulla. Donec a massa imperdiet, porta quam a, semper orci. Mauris tempor in dolor sed auctor. Ut tincidunt, urna sed sollicitudin finibus, diam turpis aliquet mauris, vel tincidunt massa ex et lacus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed ac velit ante. Integer nec eros rutrum ipsum tempus sollicitudin et id odio. 
             </p>
@@ -21,4 +21,4 @@ const About = () => {
 
 };
 
-export default About;
+export default About; 
