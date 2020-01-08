b6176de97198ef47e037992b1b863126dd2d2464
MOSvisio <lucas.schutz@vivoka.com>
2020-01-08 15:59:31 +0100

    second version

--- src/Pages/Project.js
+++ src/Pages/Project.js
@@ -5,19 +5,29 @@ import './Project.css'
 const Project = ({ image, alt, title, lien, text, techno }) => {
 
     const [flipped, set] = useState(false)
-    const { transform, opacity } = useSpring({
+    const { opacity } = useSpring({
         opacity: flipped ? 1 : 0,
-        transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
         config: {mass : 5, tension: 500, friction: 80 }
-    })    
+    })   
+    
+    
 
     return (
         <div className="rootCard">
             <div className="cardSpring" onMouseEnter={() => set(state => !state)} onMouseLeave={() => set(state =>!state)}>
-                <a.div class="c front" style={{opacity: opacity.interpolate(o => 1 - o), transform, backgroundImage: 'url(' + image + ')'}}></a.div>
-                <a.div class="c back" style={{opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`)}}>
+                <a.div class="c front" style={{/*opacity: opacity.interpolate(o => 1 - o), */backgroundImage: 'url(' + image + ')'}}>
+                    
+                </a.div>
+                <a.div class="c back" style={{opacity}}>
+                    <h3>{title}</h3>
                     <p>{text}</p>
-                    <button>Github</button>
+                    <p><strong>Langage : </strong>{techno}</p>
+                    <div >
+                        { lien &&
+                            <a rel="noopener noreferrer" href={lien} className="btn btn-primary " target="_blank">Github</a>
+                        
+                        }
+                    </div>
                 </a.div>
            </div>
         </div>
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -6,6 +6,7 @@ import sudoku from './img/sudoku.png';
 import portfolio from './img/portfolio-react.png';
 import banque from './img/banque_de_moselle.png';
 import othello from './img/othello.png';
+import morpion from './img/morpion.jpg'
 
 const Projects = () => {
     const props = useSpring({
@@ -16,38 +17,46 @@ const Projects = () => {
     return (
         <animated.div style={props} className="allProject">
             <h1>Projects</h1>
-            <div className="projects">
-                <Project image={sudoku} 
-                    text="Creation of a sudoku game, with a basic AI which can resolse basic sudoku grid" 
-                    alt="Sudoku application" 
-                    title="Sudoku resolver" 
-                    lien="https://github.com/MOSvisio/sudoku_resolver" 
-                    techno={["C++", "Qt", "SQLite"]} 
-                />
-                
-                <Project image={portfolio} 
-                    text="Creation of this portfolio in order to learn React" 
-                    alt="portfolio" 
-                    title="Portfolio React" 
-                    lien="https://github.com/MOSvisio/portfolio-react" 
-                    techno={["HTML","CSS","React JS"]} 
-                />
+            <div className="container-projects">
+                <div className="projects">
+                    <Project image={sudoku} 
+                        text="Creation of a sudoku game, with a basic AI which can resolse basic sudoku grid" 
+                        alt="Sudoku application" 
+                        title="Sudoku resolver" 
+                        lien="https://github.com/MOSvisio/sudoku_resolver" 
+                        techno="C++, Qt, SQLite"
+                    />
+                    
+                    <Project image={portfolio} 
+                        text="Creation of this portfolio in order to learn React" 
+                        alt="portfolio" 
+                        title="Portfolio React" 
+                        lien="https://github.com/MOSvisio/portfolio-react" 
+                        techno="HTML CSS React JS"
+                    />
 
-                <Project image={banque} 
-                    text="Creation of a server for a student project, the goal was to create a simple application which allow user to order something" 
-                    alt="Banque application" 
-                    title="Banque de moselle" 
-                    lien="https://github.com/MOSvisio/Projet_banque_AS_2019" 
-                    techno={["Java", "Spring", "Hibernate"]} 
-                />
+                    <Project image={banque} 
+                        text="Creation of a server for a student project, the goal was to create a simple application which allow user to order something" 
+                        alt="Banque application" 
+                        title="Banque de moselle" 
+                        lien="https://github.com/MOSvisio/Projet_banque_AS_2019" 
+                        techno="Java, Spring, Hibernate"
+                    />
 
-                <Project image={othello}
-                    text="Creation of an Othello game for a student project. The user can play with another player or against an AI" 
-                    alt="Othello application" 
-                    title="Othello game" 
-                    lien="https://github.com/MOSvisio/Divers/tree/PY_othello" 
-                    techno={["Python"]} 
-                />
+                    <Project image={othello}
+                        text="Creation of an Othello game for a student project. The user can play with another player or against an AI" 
+                        alt="Othello application" 
+                        title="Othello game" 
+                        lien="https://github.com/MOSvisio/Divers/tree/PY_othello" 
+                        techno="Python"
+                    />
+                    <Project image={morpion}
+                        text="Creation of a morpion game for mobile, in order to learn QML" 
+                        alt="Morpion" 
+                        title="Morpion game" 
+                        techno="C++, QML"
+                    />
+                </div>
             </div>
         </animated.div>
     );
