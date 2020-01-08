d907232f22127c6337f574f113547b9b10d42cbf
MOSvisio <lucas.schutz@vivoka.com>
2020-01-08 10:40:55 +0100

    card first commit

--- src/Pages/Project.js
+++ src/Pages/Project.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSpring, animated as a } from 'react-spring'
 import './Project.css'
 
 const Project = ({ image, alt, title, lien, text, techno }) => {
+
+    const [flipped, set] = useState(false)
+    const { transform, opacity } = useSpring({
+        opacity: flipped ? 1 : 0,
+        transform: `perspective(600px) rotateX(${flipped ? 180 : 0} deg)`,
+        config: {mass : 5, tension: 500, friction: 80 }
+    })    
 
     return (
         <div className="project">
