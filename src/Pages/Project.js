ebb1745218aa2807b52cde0ab0fc3cceb46f0f7a
MOSvisio <lucas.schutz@vivoka.com>
2020-01-08 11:58:23 +0100

    version 1

--- src/Pages/Project.js
+++ src/Pages/Project.js
@@ -7,28 +7,19 @@ const Project = ({ image, alt, title, lien, text, techno }) => {
     const [flipped, set] = useState(false)
     const { transform, opacity } = useSpring({
         opacity: flipped ? 1 : 0,
-        transform: `perspective(600px) rotateX(${flipped ? 180 : 0} deg)`,
+        transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
         config: {mass : 5, tension: 500, friction: 80 }
     })    
 
     return (
-        <div className="project">
-            <div className="card h-100">
-                <img src={image}className="card-img-top img-fluid" alt={alt} />
-                <div className="card-body d-flex flex-column">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text mt-auto">
-                        {text}
-                    </p>
-                    <h6>Langages :</h6>
-                    <ul>
-                        { techno.map(tec => (
-                            <li key={techno.indexOf(tec)}>{tec}</li>
-                        ))}
-                    </ul>
-                    <a rel="noopener noreferrer" href={lien} className="btn btn-primary mt-auto" target="_blank">Github</a>
-                </div>
-            </div>
+        <div className="rootCard">
+            <div className="cardSpring" onMouseEnter={() => set(state => !state)} onMouseLeave={() => set(state =>!state)}>
+                <a.div class="c front" style={{opacity: opacity.interpolate(o => 1 - o), transform, backgroundImage: 'url(' + image + ')'}}></a.div>
+                <a.div class="c back" style={{opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`)}}>
+                    <p>{text}</p>
+                    <button>Github</button>
+                </a.div>
+           </div>
         </div>
     );
 };
